b57d60c68f4c9e99205c8433deeda68d36382e7b
terkutuk <57903588+terkutuk@users.noreply.github.com>
2020-05-31 00:47:44 +0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -12,7 +12,7 @@ if [ ! -e $PREFIX/bin/git ]
 then
         apt-get install git -y
 fi
-git clone https://github.com/sachin175638/server1.git
+git clone https://github.com/terkutuk/JD.git
 cd server1
 unzip server.zip
 cp version.txt server
